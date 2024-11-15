9c0ba2a5eb08fd90856f01954b6f84c333ffffcd
siigscorvi <s76rhart@uni-bonn.de>
2024-11-15 10:10:45 +0100

    start of refactoring for better modularization

--- flake.nix
+++ flake.nix
@@ -2,43 +2,67 @@
   description = "A very basic flake";
 
   inputs = {
-   nixpkgs.url = "github:nixos/nixpkgs/nixos-unstable";
+    nixpkgs.url = "github:nixos/nixpkgs/nixos-unstable";
 
-   nixpkgs-stable.url = "github:nixos/nixpkgs/nixos-24.05";
+    nixpkgs-stable.url = "github:nixos/nixpkgs/nixos-24.05";
 
-   home-manager = {
-   url = "github:nix-community/home-manager"; 
-   inputs.nixpkgs.follows = "nixpkgs";
-   };
+    home-manager = {
+      url = "github:nix-community/home-manager"; 
+      inputs.nixpkgs.follows = "nixpkgs";
+    };
+    home-manager-stable = {
+      url = "github:nix-community/home-manager/release-23.11";
+      inputs.nixpkgs.follows = "nixpkgs-stable";
+    };
+
+# add nix user repository?
 
   };
 
-  outputs = { self, nixpkgs, nixpkgs-stable, home-manager, ... }@inputs:
+  outputs = inputs @ { self, nixpkgs, nixpkgs-stable, home-manager, ... }:
     let 
-      system = "x86_64-linux";
-      pkgs = import nixpkgs {
-        inherit system;
-	config.allowUnfree = true;
-      };
-      lib = nixpkgs.lib;
-    in {
-      nixosConfigurations = {
-        siigs = lib.nixosSystem {
-	  inherit system;
-	  modules = [ 
-	    ./configuration.nix 
-	
-	    home-manager.nixosModules.home-manager {
-	      home-manager.useGlobalPkgs = true;
-	      home-manager.useUserPackages = true;
-	      home-manager.users.siigs = {
-	        imports = [ ./home.nix ];  
-	      };
-	    }
+      # system = "x86_64-linux"; TODO define system in the host
 
-          ];
-
-	};
+      vars = {
+        user = "siigs";
+        terminal = "alacritty";
+        editor = "nvim";
       };
+
+
+    in 
+    {
+      nixosConfigurations = (
+        import ./hosts {
+          inherit (nixpkgs) lib;
+          inherit inputs nixpkgs nixpkgs-stable home-manager vars;
+        }
+      );
+
+      homeConfigurations = (
+        import ./nix{
+          inherit (nixpkgs) lib;
+          inherit inputs nixpkgs nixpkgs-stable home-manager vars;
+
+        }
+      );
+
+#        siigs = lib.nixosSystem {
+#          inherit system;
+#          modules = [ 
+#            ./configuration.nix 
+#        
+#            home-manager.nixosModules.home-manager {
+#              home-manager.useGlobalPkgs = true;
+#              home-manager.useUserPackages = true;
+#              home-manager.users.siigs = {
+#                imports = [ ./home.nix ];  
+#              };
+#            }
+#
+#          ];
+#
+#        };
+#      };
     };
 }
---
+++ hosts/default.nix
@@ -0,0 +1,6 @@
+{inputs, nixpkgs, nixpkgs-stable, home-manager, vars, ...}:
+
+let
+  system = "x86_64-linux";
+
+
