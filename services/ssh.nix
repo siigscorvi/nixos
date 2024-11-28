8093f9a52950fbc6e93acf7ce1bae9f879a183ad
siigscorvi <s76rhart@uni-bonn.de>
2024-11-28 18:05:54 +0100

    added moses key

--- services/ssh.nix
+++ services/ssh.nix
@@ -15,6 +15,7 @@
 
   users.users.siigs.openssh.authorizedKeys.keys = [
     "ssh-ed25519 AAAAC3NzaC1lZDI1NTE5AAAAIPVeo7HfiwprBHKJC55YbFf7uLfd5+7bGw0KucZ+lIb+ ruven@W51195"
+    "ssh-ed25519 AAAAC3NzaC1lZDI1NTE5AAAAIHid9tjWkf1B0yxTRJZbKIaajmbV8jW6dDu0ITKUwyPQ siigs@moses"
 
   ];
 }
