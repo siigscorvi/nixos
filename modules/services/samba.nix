f46ca4dab6911f641de8afa48aab99aa7d6c5c0f
siigscorvi <s76rhart@uni-bonn.de>
2024-12-09 00:31:13 +0100

    start of making smb more usable

--- modules/services/samba.nix
+++ modules/services/samba.nix
@@ -1,10 +1,40 @@
-{ pkgs, ... }:
+{ pkgs, vars, ... }:
 {
   services.samba.enable = true;
   # For mount.cifs, required unless domain name resolution is not needed.
   environment.systemPackages = [ pkgs.cifs-utils ];
   fileSystems."/mnt/share" = {
     device = "//192.168.42.3/shared";
+    fsType = "cifs";
+    options = let
+      # this line prevents hanging on network split
+      automount_opts = "x-systemd.automount,noauto,x-systemd.idle-timeout=60,x-systemd.device-timeout=5s,x-systemd.mount-timeout=5s";
+
+    in ["${automount_opts},credentials=/etc/nixos/smb-secrets,uid=1000,gid=1000"];
+  };
+
+  fileSystems."/home/${vars.username}/images" = {
+    device = "//192.168.42.3/shared/images";
+    fsType = "cifs";
+    options = let
+      # this line prevents hanging on network split
+      automount_opts = "x-systemd.automount,noauto,x-systemd.idle-timeout=60,x-systemd.device-timeout=5s,x-systemd.mount-timeout=5s";
+
+    in ["${automount_opts},credentials=/etc/nixos/smb-secrets,uid=1000,gid=1000"];
+  };
+
+  fileSystems."/home/${vars.username}/storage" = {
+    device = "//192.168.42.3/shared/storage";
+    fsType = "cifs";
+    options = let
+      # this line prevents hanging on network split
+      automount_opts = "x-systemd.automount,noauto,x-systemd.idle-timeout=60,x-systemd.device-timeout=5s,x-systemd.mount-timeout=5s";
+
+    in ["${automount_opts},credentials=/etc/nixos/smb-secrets,uid=1000,gid=1000"];
+  };
+
+  fileSystems."/home/${vars.username}/documents" = {
+    device = "//192.168.42.3/shared/documents";
     fsType = "cifs";
     options = let
       # this line prevents hanging on network split
