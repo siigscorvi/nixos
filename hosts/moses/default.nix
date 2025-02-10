e2d6a8261ea2815e20b931a72aa48a121f3c5dd5
siigscorvi <s76rhart@uni-bonn.de>
2025-02-10 16:44:56 +0100

    typo

--- hosts/moses/default.nix
+++ hosts/moses/default.nix
@@ -37,7 +37,7 @@
 
       #Optional helps save long term battery health
       START_CHARGE_THRESH_BAT0 = 75; # 75 and bellow it starts to charge
-      STOP_CHARGE_THRESH_BAT0 90= ; # 90 and above it stops charging
+      STOP_CHARGE_THRESH_BAT0 = 90; # 90 and above it stops charging
 
     };
   };
