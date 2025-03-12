0ae633fb8fb87721075a37980445c2867cf23e4b
siigscorvi <s76rhart@uni-bonn.de>
2025-03-12 15:44:39 +0100

    configured xsecurelock

--- modules/desktops/lock.nix
+++ modules/desktops/lock.nix
@@ -9,4 +9,15 @@
     ];
     lockerCommand = "${pkgs.xsecurelock}/bin/xsecurelock";
   };
+  
+  # xsecurelock configuration
+  environment.variables = {
+    XSECURELOCK_AUTH_BACKGROUND_COLOR = "#282828";
+    XSECURELOCK_AUTH_FOREGROUND_COLOR = "#fbf1c7";
+    XSECURELOCK_AUTH_TIMEOUT = 30;
+    XSECURELOCK_BLANK_TIMEOUT = 30;
+    XSECURELOCK_FONT = "JetBrainsMonoNerdFont";
+    XSECURELOCK_COMPOSITE_OBSCURER = 0;
+  };
+
 }
