8938272f2fe1afc1b120afed54524a42ad943dfe
siigscorvi <s76rhart@uni-bonn.de>
2025-03-31 17:57:24 +0200

    added more info with gitsigns

--- config/nvim/plugin/gitsigns.lua
+++ config/nvim/plugin/gitsigns.lua
@@ -34,4 +34,17 @@ require('gitsigns').setup {
     virt_text_priority = 100,
     use_focus = true,
   },
+  current_line_blame_formatter = '<author>, <author_time:%R> - <summary>',
+  sign_priority = 6,
+  update_debounce = 100,
+  status_formatter = nil, -- Use default
+  max_file_length = 40000, -- Disable if file is longer than this (in lines)
+  preview_config = {
+    -- Options passed to nvim_open_win
+    border = 'single',
+    style = 'minimal',
+    relative = 'cursor',
+    row = 0,
+    col = 1
+  },
 }
