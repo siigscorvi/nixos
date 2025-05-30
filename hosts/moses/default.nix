e2004c479c340092dd91f14a99a94163ced9f6c3
siigscorvi <s76rhart@uni-bonn.de>
2025-05-30 12:27:05 +0200

    refactor(moses)!: removed old pg stuff

--- hosts/moses/default.nix
+++ hosts/moses/default.nix
@@ -29,9 +29,6 @@
     autostart = true;
   };
 
-  nixpkgs.config.permittedInsecurePackages = [ "segger-jlink-qt4-810" ];
-  nixpkgs.config.segger-jlink.acceptLicense = true;
-
   # default dir setup
   environment.sessionVariables = {
     XDG_DESKTOP_DIR = "$HOME/documents/";
@@ -39,7 +36,7 @@
     XDG_DOWNLOAD_DIR = "$HOME/downloads";
     XDG_MUSIC_DIR = "$HOME/music";
     XDG_PICTURES_DIR = "$HOME/images";
-    XDG_PUBLICSHARE_DIR="$HOME/nc";
+    XDG_PUBLICSHARE_DIR = "$HOME/nc";
     XDG_VIDEOS_DIR = "$HOME/videos";
     XDG_CONFIG_HOME = "$HOME/.config";
   };
