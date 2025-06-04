bee6e784139fde3455c8f16b82bddaa2863df093
siigscorvi <s76rhart@uni-bonn.de>
2025-06-04 10:14:04 +0200

    feat: spotify_player
    spotify_player which is a tui to control spotify, but also a daemon to run spotify player in the background with minimal ressources

--- modules/programs/spotify.nix
+++ modules/programs/spotify.nix
@@ -1,7 +1,17 @@
-{ pkgs, ... }:
+{ pkgs, vars, ... }:
 
 {
   environment.systemPackages = with pkgs; [
     spotify
+    (spotify-player.override {
+      withAudioBackend = "pulseaudio";
+      withMediaControl = true;
+      withImage = true;
+      withDaemon = true;
+      withNotify = true;
+      withStreaming = true;
+      withSixel = true;
+      withFuzzy = true;
+    })
   ];
 }
