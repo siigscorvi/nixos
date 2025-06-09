dad7c4b22ddb324174a21a3b4d941a2d51391210
siigscorvi <s76rhart@uni-bonn.de>
2025-06-09 08:53:08 +0200

    refactor(programs): otherProgams.nix now in default

--- modules/programs/default.nix
+++ modules/programs/default.nix
@@ -9,6 +9,27 @@
     ./btop.nix
     ./tmux.nix
     ./thunderbird.nix
-    ./otherPrograms.nix
+  ];
+
+  environment.systemPackages = with pkgs; [
+    drawio
+    anki
+
+    pandoc
+    texlive.combined.scheme-full
+    discord
+    vlc
+    firefox
+
+    xournalpp
+    openvpn
+    openssl
+    # this needs to move to a module!
+    rofi
+    arandr
+
+    python313
+
+    zathura
   ];
 }
