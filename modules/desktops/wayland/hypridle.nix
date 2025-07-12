1f1be57becd608e29fca5dbce75988213bfac15c
siigscorvi <s76rhart@uni-bonn.de>
2025-07-12 23:13:11 +0200

    fix(desktop): missing funcs in hypridle
    due to nix not finding packages, hypridle did not work properly e.g. dimming the screen

--- modules/desktops/wayland/hypridle.nix
+++ modules/desktops/wayland/hypridle.nix
@@ -10,34 +10,35 @@ in {
       description = "Enable Hypridle for managing idle states in Hyprland";
     };
     lockwarning-timeout = mkOption {
-        type = types.int;
-        default = 100;
-        description = "Timeout in seconds before showing a lock warning";
+      type = types.int;
+      default = 100;
+      description = "Timeout in seconds before showing a lock warning";
     };
     screendimming-beforelock-timeout = mkOption {
-        type = types.int;
-        default = 110;
-        description = "Timeout in seconds before dimming the screen";
+      type = types.int;
+      default = 110;
+      description = "Timeout in seconds before dimming the screen";
     };
     lock-timeout = mkOption {
-        type = types.int;
-        default = 120;
-        description = "Timeout in seconds before the locking the session";
+      type = types.int;
+      default = 120;
+      description = "Timeout in seconds before the locking the session";
     };
     screendimming-locked-timeout = mkOption {
-        type = types.int;
-        default = 10;
-        description = "Timeout in seconds before dimming the screen while locked";
+      type = types.int;
+      default = 10;
+      description = "Timeout in seconds before dimming the screen while locked";
     };
     blankscreen-timeout = mkOption {
-        type = types.int;
-        default = 30;
-        description = "Timeout in seconds before blanking the screen while locked";
+      type = types.int;
+      default = 30;
+      description =
+        "Timeout in seconds before blanking the screen while locked";
     };
     sleep-timeout = mkOption {
-        type = types.int;
-        default = 180;
-        description = "Timeout in seconds before going to sleep while locked";
+      type = types.int;
+      default = 180;
+      description = "Timeout in seconds before going to sleep while locked";
     };
   };
 
@@ -48,65 +49,77 @@ in {
         text = ''
           general {
             lock_cmd = pidof ${pkgs.hyprlock}/bin/hyprlock || ${pkgs.hyprlock}/bin/hyprlock
-            before_sleep_cmd = loginctl lock-session
-            after_sleep_cmd = hyprctl dispatch dpms on
+            before_sleep_cmd = ${pkgs.systemd}/bin/loginctl lock-session
+            after_sleep_cmd = ${pkgs.hyprland}/bin/hyprctl dispatch dpms on
 
             ignore_dbus_inhibit = false
             ignore_systemd_inhibit = false
           }
 
-          ${if cfg.lockwarning-timeout == 0 then "" else ''
-          # idle warning
-          listener {
-            timeout = ${toString cfg.lockwarning-timeout}
-            on-timeout = notify-send "session about to be locked"
-          }
+          ${if cfg.lockwarning-timeout == 0 then
+            ""
+          else ''
+            # idle warning
+            listener {
+              timeout = ${toString cfg.lockwarning-timeout}
+              on-timeout = ${pkgs.libnotify}/bin/notify-send "session about to be locked"
+            }
           ''}
 
-          ${if cfg.screendimming-beforelock-timeout == 0 then "" else ''
-          # while not locked, save brightness and drim screen
-          # return brightness on input
-          listener {
-            timeout = ${toString cfg.screendimming-beforelock-timeout}
-            on-timeout = pidof ${pkgs.hyprlock}/bin/hyprlock || brillo -O && brillo -S 10%
-            on-resume = pidof ${pkgs.hyprlock}/bin/hyprlock || brillo -I
-          }
+          ${if cfg.screendimming-beforelock-timeout == 0 then
+            ""
+          else ''
+            # while not locked, save brightness and drim screen
+            # return brightness on input
+            listener {
+              timeout = ${toString cfg.screendimming-beforelock-timeout}
+              on-timeout = pidof ${pkgs.hyprlock}/bin/hyprlock || ${pkgs.brillo}/bin/brillo -O && ${pkgs.brillo}/bin/brillo -S 10%
+              on-resume = pidof ${pkgs.hyprlock}/bin/hyprlock || ${pkgs.brillo}/bin/brillo -I
+            }
           ''}
 
-          ${if cfg.lock-timeout == 0 then "" else ''
-          # lock session
-          listener {
-            timeout = ${toString cfg.lock-timeout}
-            on-timeout = loginctl lock-session
-          }
+          ${if cfg.lock-timeout == 0 then
+            ""
+          else ''
+            # lock session
+            listener {
+              timeout = ${toString cfg.lock-timeout}
+              on-timeout = ${pkgs.systemd}/bin/loginctl lock-session
+            }
           ''}
 
-          ${if cfg.screendimming-locked-timeout == 0 then "" else ''
-          # while locked, dim screen
-          # return brightness on input
-          listener {
-            timeout = ${toString cfg.screendimming-locked-timeout}
-            on-timeout = pidof ${pkgs.hyprlock}/bin/hyprlock && brillo -S 10%
-            on-resume = pidof ${pkgs.hyprlock}/bin/hyprlock && brillo -I
-          }
+          ${if cfg.screendimming-locked-timeout == 0 then
+            ""
+          else ''
+            # while locked, dim screen
+            # return brightness on input
+            listener {
+              timeout = ${toString cfg.screendimming-locked-timeout}
+              on-timeout = pidof ${pkgs.hyprlock}/bin/hyprlock && ${pkgs.brillo}/bin/brillo -S 10%
+              on-resume = pidof ${pkgs.hyprlock}/bin/hyprlock && ${pkgs.brillo}/bin/brillo -I
+            }
           ''}
 
-          ${if cfg.blankscreen-timeout == 0 then "" else ''
-          # while locked, turn off the display
-          # turn display back on on input
-          listener {
-            timeout = ${toString cfg.blankscreen-timeout}
-            on-timeout = pidof ${pkgs.hyprlock}/bin/hyprlock && hyprctl dispatch dpms off
-            on-resume = hyprctl dispatch dpms on
-          }
+          ${if cfg.blankscreen-timeout == 0 then
+            ""
+          else ''
+            # while locked, turn off the display
+            # turn display back on on input
+            listener {
+              timeout = ${toString cfg.blankscreen-timeout}
+              on-timeout = pidof ${pkgs.hyprlock}/bin/hyprlock && ${pkgs.hyprland}/bin/hyprctl dispatch dpms off
+              on-resume = ${pkgs.hyprland}/bin/hyprctl dispatch dpms on
+            }
           ''}
 
-          ${if cfg.sleep-timeout == 0 then "" else ''
-          # while locked, suspend
-          listener {
-            timeout = ${toString cfg.sleep-timeout}
-            on-timeout = pidof ${pkgs.hyprlock}/bin/hyprlock && systemctl suspend
-          }
+          ${if cfg.sleep-timeout == 0 then
+            ""
+          else ''
+            # while locked, suspend
+            listener {
+              timeout = ${toString cfg.sleep-timeout}
+              on-timeout = pidof ${pkgs.hyprlock}/bin/hyprlock && ${pkgs.systemd}/bin/systemctl suspend
+            }
           ''}
         '';
       };
