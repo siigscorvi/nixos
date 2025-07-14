a4deb93e259b804c8a2666f512b8d12142584ad7
siigscorvi <s76rhart@uni-bonn.de>
2025-07-14 16:17:01 +0200

    feat(scripts): added hypridle to hypr-reload

--- scripts/hyprland/hypr-reload.nix
+++ scripts/hyprland/hypr-reload.nix
@@ -1,11 +1,12 @@
-{ pkgs, vars, ... }:
+{ pkgs, ... }:
 
 let
   hypr-reload = pkgs.writeShellScriptBin "hypr-reload" ''
     hyprctl reload > /tmp/reload-log.txt
     systemctl --user restart waybar > /tmp/reload-log.txt
+    systemctl --user restart hypridle.service > /tmp/reload-log.txt
     hyprctl hyprpaper reload ,"~/.config/wallpapers/current" > /tmp/reload-log.txt
-    notify-send "Reload Finished" "See /tmp/reload-log.txt for more info"
+    notify-send "Reload finished" "see /tmp/reload-log.txt for more info"
   '';
 
 in {
