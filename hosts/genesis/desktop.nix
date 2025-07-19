bc0763f7b7f3b1fef1de9061e33e8d4addf59716
siigscorvi <s76rhart@uni-bonn.de>
2025-07-19 12:00:48 +0200

    feat(genesis): setup desktop.nix file

--- hosts/genesis/desktop.nix
+++ hosts/genesis/desktop.nix
@@ -1,36 +1,50 @@
 {
   config.system.desktop = {
     hyprland = {
-        monitors = ''
-          monitor=desc:Lenovo Group Limited R27qe UP331C79,preferred,auto-up,1.0666666
-          monitor=,preferred,auto-left,1
-        '';
-        workspace-rules = ''
-          # workspaces
-          workspace = name:1, monitor:eDP-1, default:true
-          workspace = name:2, monitor:desc:Lenovo Group Limited R27qe UP331C79, default:true
-          workspace = name:3, monitor:desc:Lenovo Group Limited R27qe UP331C79
-          workspace = name:4, monitor:desc:Lenovo Group Limited R27qe UP331C79
-          workspace = name:5, monitor:desc:Lenovo Group Limited R27qe UP331C79
-          workspace = name:6, monitor:desc:Lenovo Group Limited R27qe UP331C79
-          workspace = name:7, monitor:desc:Lenovo Group Limited R27qe UP331C79
-          workspace = name:8, monitor:eDP-1,
-          workspace = name:9, monitor:eDP-1,
-          workspace = name:0, monitor:desc:Lenovo Group Limited R27qe UP331C79
-        '';
-        inactive-transparency = 0.9;
-        blur = true;
-        shadow = true;
-        animations = true;
-      };
+
+      monitors = ''
+        monitor=desc:Lenovo Group Limited R27qe UP331C79,2560x1440@180.08,auto-up,1
+        monitor=desc:Acer Technologies GN276HL T6BEE0018501,preferred,auto-left,1
+      '';
+
+      workspace-rules = ''
+        # workspaces
+        workspace = name:1, monitor:desc:Lenovo Group Limited R27qe UP331C79, default:true
+        workspace = name:2, monitor:desc:Acer Technologies GN276HL T6BEE0018501, default:true
+        workspace = name:3, monitor:desc:Acer Technologies GN276HL T6BEE0018501
+        workspace = name:4, monitor:desc:Acer Technologies GN276HL T6BEE0018501
+        workspace = name:5, monitor:desc:Acer Technologies GN276HL T6BEE0018501
+        workspace = name:6, monitor:desc:Acer Technologies GN276HL T6BEE0018501
+        workspace = name:7, monitor:desc:Acer Technologies GN276HL T6BEE0018501
+        workspace = name:8, monitor:desc:Lenovo Group Limited R27qe UP331C79
+        workspace = name:9, monitor:desc:Lenovo Group Limited R27qe UP331C79
+        workspace = name:0, monitor:desc:Acer Technologies GN276HL T6BEE0018501
+      '';
+
+      extra-settings = ''
+        windowrule = opacity 0.8, class:^(kitty)$
+      '';
+      inactive-transparency = 1.0;
+      blur = true;
+      shadow = true;
+      animations = true;
+    };
+
+    hypridle = {
+      lockwarning-timeout = 260;
+      screendimming-beforelock-timeout = 270;
+      lock-timeout = 300;
+      screendimming-locked-timeout = 20;
+      blankscreen-timeout = 30;
+      sleep-timeout = 299;
+    };
 
     hyprlock.background.style = "screenshot";
 
     waybar = {
       battery = false;
-      output = "eDP-1";
+      output = "DP-3";
     };
-
 
   };
 }
